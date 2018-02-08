2914b6e6749bf4b454587113e301a2b16e60dd3b
kameush <nico9017@gmail.com>
2018-02-08 14:13:02 +0100

    Changes to server.js. Debugging route POST /users

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
-require('./config/config');
+require('./config/config')
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
-const path = require('path');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -39,29 +39,37 @@ app.get('/todos', (req, res) => {
 
 app.get('/todos/:id', (req, res) => {
   let id = req.params.id;
+
   if (!ObjectID.isValid(id)) {
     res.status(404).send();
   };
+
   Todo.findById(id).then((todo) => {
     if (!todo) {
       res.status(404).send();
     }
+
     res.send({ todo });
-  }).catch((e) => console.log(404, e));
+  }).catch(e => res.status(400).send());
 });
 
 app.delete('/todos/:id', (req, res) => {
   let id = req.params.id;
+
   if (!ObjectID.isValid(id)) {
     res.status(404).send();
   }
+
   Todo.findByIdAndRemove(id).then((todo) => {
     if (!todo) {
       res.status(404).send();
     }
-    res.status(200).send({todo});
-  }).catch((e) => console.log(400));
-})
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
 
 app.patch('/todos/:id', (req, res) => {
   let id = req.params.id;
@@ -82,11 +90,25 @@ app.patch('/todos/:id', (req, res) => {
     if (!todo) {
       return res.status(404).send();
     }
+
     res.send({todo});
   }).catch((e) => {
     res.status(400).send();  
   });
+});
 
+// POST /users
+app.post('/users', (req, res) => {
+  let body = _.pick(req.body, ['email', 'password']);
+  let user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
 });
 
 app.listen(port, () => {
@@ -94,4 +116,4 @@ app.listen(port, () => {
 });
 
 
-module.exports = { app }; 
+module.exports = { app };
