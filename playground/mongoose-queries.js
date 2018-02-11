d8146ba807166e18d4a04d9df7727895c86d52dc
kameush <nico9017@gmail.com>
2018-02-11 21:41:16 +0100

    Add POST /users/login route

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -3,11 +3,12 @@ const { ObjectID } = require('mongodb');
 const { mongoose } = require('./../server/db/mongoose');
 const { User } = require('./../server/models/user');
 
-const id = '5a521ed8819c264d2dbebdf9';
+//const id = '5a521ed8819c264d2dbebdf9';
+const id = "5a7d03880343f98e291b862e";
 
 User.findById(id).then((user) => {
   if(!user) {
     return console.log('Id not found');
   }
   console.log('User by Id', user);
-}).catch((e) => console.log(e));
+}).catch((e) => console.log('ERROR *****', e));
